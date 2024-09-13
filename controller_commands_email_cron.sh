8d8b72d375148e8359100c37715c1211439f2014
Jeff Hagley <jhagley@piergroup.com>
2024-09-13 15:53:46 -0400

    controller_commands_email_cron.sh

---
+++ controller_commands_email_cron.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set this to be the path to the arubaos_crash_detection folder
+PATH_TO_ARUBAOS_CRASH_DETECTION="/path/to/arubaos_crash_detection"
+EMAIL="test@email.com"
+
+python3 ${PATH_TO_ARUBAOS_CRASH_DETECTION}/controller_commands.py > ${PATH_TO_ARUBAOS_CRASH_DETECTION}/controller_commands_output
+
+
+if [ -s ${PATH_TO_ARUBAOS_CRASH_DETECTION}/controller_crash_output ]; then
+        # The file is not-empty.
+        mail -s "Aruba Controller Commands" ${EMAIL} < ${PATH_TO_ARUBAOS_CRASH_DETECTION}/controller_commands_output
+        rm ${PATH_TO_ARUBAOS_CRASH_DETECTION}/controller_commands_output
+fi
